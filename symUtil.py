e752a0ab47da9d9b34b5ce6f5cd40ac98977ec6e
David Durst <ddurst@mozilla.com>
2016-06-17 13:57:52 +0100

    Refactor out the re. It's not necessary to regex the replacement of an explicitly checked string in an explicit location. This should be simpler.

--- symUtil.py
+++ symUtil.py
@@ -1,5 +1,4 @@
 import os
-import re
 
 def mkdir_p(path):
   if not os.path.exists(path):
@@ -8,6 +7,6 @@ def mkdir_p(path):
 def GetSymbolFileName(libName):
   # Guess the name of the .sym file on disk
   if libName[-4:] == ".pdb":
-    return re.sub(r"\.[^\.]+$", ".sym", libName)
+    return libName[:-4] + ".sym"
   return libName + ".sym"
 
